b111d4d627fb13cd226513868ba00d5b542ce215
SamuelGivency <givencysamuel1@gmail.com>
2021-11-01 20:25:02 +0700

    test

---
+++ luaskelilingPython.py
@@ -0,0 +1,8 @@
+phi = 3.14
+jari = float(input("Masukkan panjang jari-jari lingkaran: "))
+luas = phi*jari*jari
+keliling = 2*phi*jari
+
+print("\nLuas lingkaran adalah : "+ str(luas))
+print("keliling lingkaran adalah : "+ str(keliling))
+
